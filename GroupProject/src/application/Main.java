337ec2683a20ded2948a2651db85d76b8f23aeb7
Theresa Crawford <70966378+crawlive@users.noreply.github.com>
2020-10-16 23:15:33 -0500

    COMMIT: Notes, events, nav bar, title and navigation

--- GroupProject/src/application/Main.java
+++ GroupProject/src/application/Main.java
@@ -12,8 +12,9 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("FrontPage.fxml"));
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root,600,450);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setTitle("Assignment Tracker");
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
