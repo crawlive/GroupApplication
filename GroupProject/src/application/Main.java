329031b0b584135b33c4ac448610770a01de2bbe
vgerber614 <43424164+vgerber614@users.noreply.github.com>
2020-11-02 19:22:37 -0600

    Make Front Page fullscreen with StackPane root.

--- GroupProject/src/application/Main.java
+++ GroupProject/src/application/Main.java
@@ -3,7 +3,7 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
 import javafx.fxml.FXMLLoader;
 
 
@@ -11,13 +11,15 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("frontPage/FrontPage.fxml"));
-			Scene scene = new Scene(root,600,450);
+			StackPane root = (StackPane)FXMLLoader.load(getClass().getResource("frontPage/FrontPage.fxml"));
+			Scene scene = new Scene(root,1440,880);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setTitle("Assignment Tracker");
+			
+			// opens full screen
+		    primaryStage.setMaximized(true);
 			primaryStage.setScene(scene);
 			primaryStage.show();
-			primaryStage.setResizable(false);
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
