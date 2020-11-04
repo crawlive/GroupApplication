b784f2c7748fe9b9bee319f502e3c14ca5b9ba9a
vgerber614 <43424164+vgerber614@users.noreply.github.com>
2020-11-04 15:36:59 -0600

    Switch scenes from FrontPage

--- GroupProject/src/application/controller/FrontPageController.java
+++ GroupProject/src/application/controller/FrontPageController.java
@@ -1,5 +1,56 @@
 package application.controller;
 
+import java.io.IOException;
+
+import application.Main;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
 public class FrontPageController {
-	//will hold methods that do stuff
+
+    @FXML
+    private ToggleGroup todo_date_toggle;
+
+    @FXML
+    private Button courseBtn;
+
+    @FXML
+    void openAddTask(ActionEvent event) throws IOException {
+		FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(Main.class.getResource("view/NewAssignment.fxml"));
+        AnchorPane newPane = loader.load();
+        Scene scene = new Scene(newPane);
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        window.setScene(scene);
+        window.show();
+    }
+
+    @FXML
+    void openAddCourse(ActionEvent event) throws IOException {
+		FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(Main.class.getResource("view/AddCourse.fxml"));
+        AnchorPane newPane = loader.load();
+        Scene scene = new Scene(newPane);
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        window.setScene(scene);
+        window.show();
+    }
+
+    @FXML
+    void openEditCourse(ActionEvent event) throws IOException {
+		FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(Main.class.getResource("view/EditCourse.fxml"));
+        AnchorPane newPane = loader.load();
+        Scene scene = new Scene(newPane);
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        window.setScene(scene);
+        window.show();
+    }
 }
