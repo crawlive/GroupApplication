e9699351388ace65c7f2237edb3312a4cb0249e8
vgerber614 <43424164+vgerber614@users.noreply.github.com>
2020-11-27 22:48:15 -0600

    Populate list view with data from model.

--- GroupProject/src/application/controller/FrontPageController.java
+++ GroupProject/src/application/controller/FrontPageController.java
@@ -4,7 +4,9 @@
 package application.controller;
 
 import application.Main;
+import application.model.MainModel;
 import application.model.Task;
+import application.model.TextModel;
 
 import java.io.IOException;
 import java.net.URL;
@@ -32,232 +34,220 @@ import javafx.stage.StageStyle;
 
 public class FrontPageController implements Initializable {
 
-    @FXML
-    private ToggleGroup todo_date_toggle;
+	@FXML
+	private ToggleGroup todo_date_toggle;
 
-    @FXML
-    private Button courseBtn;
-    
-    @FXML
-    private Label noEventsMsg; // Text that shows when there are no events
-    
-    @FXML
-    private TitledPane todoPane, completedPane;
-    
-    @FXML
-    private ListView<Task> todoListView, completedListView, dateListView, eventsListView;
-    
-    private ObservableList<Task> todoList;
-    private ObservableList<Task> completedTasks;
-    private ObservableList<Task> listByDate;
-    private ObservableList<Task> events;
+	@FXML
+	private Button courseBtn;
 
-  //------------- SCENE CHANGES ----------------//
-    
-    /*
-     * openAddTask
-     *
-     * Opens add task scene when user clicks new task button
-     */
-    @FXML
-    void openAddTask(ActionEvent event) throws IOException {
+	@FXML
+	private Label noEventsMsg; // Text that shows when there are no events
 
-    	FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(Main.class.getResource("view/NewAssignment.fxml"));
+	@FXML
+	private TitledPane todoPane, completedPane;
 
-        AnchorPane modalPane = loader.load();
-        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
-        Stage modal = initModal(modalPane, window);
+	@FXML
+	private ListView<Task> todoListView, completedListView, dateListView, eventsListView;
 
-        NewAssignmentController controller = loader.getController();
-        controller.passStages(modal, window);
+	private ObservableList<Task> todoList;
+	private ObservableList<Task> completedTasks;
+	private ObservableList<Task> listByDate;
+	private ObservableList<Task> events;
 
-        modal.show();
+	// ------------- SCENE CHANGES ----------------//
 
-    }
+	/*
+	 * openAddTask
+	 *
+	 * Opens add task scene when user clicks new task button
+	 */
+	@FXML
+	void openAddTask(ActionEvent event) throws IOException {
 
-
-    /*
-     * openAddCourse
-     *
-     * Opens add course scene when user clicks add course button
-     */
-    @FXML
-    void openAddCourse(ActionEvent event) throws IOException {
 		FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(Main.class.getResource("view/AddCourse.fxml"));
+		loader.setLocation(Main.class.getResource("view/NewAssignment.fxml"));
 
-        AnchorPane modalPane = loader.load();
-        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
-        Stage modal = initModal(modalPane, window);
+		AnchorPane modalPane = loader.load();
+		Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		Stage modal = initModal(modalPane, window);
 
-        AddCourseController controller = loader.getController();
-        controller.passStages(modal, window);
+		NewAssignmentController controller = loader.getController();
+		controller.passStages(modal, window);
 
-        modal.show();
-    }
+		modal.show();
 
+	}
 
-    /*
-     * openEditCourse
-     *
-     * Opens edit course scene for a particular course when user clicks an existing course button
-     */
-    @FXML
-    void openEditCourse(ActionEvent event) throws IOException {
+	/*
+	 * openAddCourse
+	 *
+	 * Opens add course scene when user clicks add course button
+	 */
+	@FXML
+	void openAddCourse(ActionEvent event) throws IOException {
 		FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(Main.class.getResource("view/EditCourse.fxml"));
+		loader.setLocation(Main.class.getResource("view/AddCourse.fxml"));
 
-        AnchorPane modalPane = loader.load();
-        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
-        Stage modal = initModal(modalPane, window);
+		AnchorPane modalPane = loader.load();
+		Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		Stage modal = initModal(modalPane, window);
 
-        EditCourseController controller = loader.getController();
-        controller.passStages(modal, window);
+		AddCourseController controller = loader.getController();
+		controller.passStages(modal, window);
 
-        // TODO: use EditCourse controller too pass in course info for edit
-        // ...
+		modal.show();
+	}
 
-        modal.show();
-    }
+	/*
+	 * openEditCourse
+	 *
+	 * Opens edit course scene for a particular course when user clicks an existing
+	 * course button
+	 */
+	@FXML
+	void openEditCourse(ActionEvent event) throws IOException {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(Main.class.getResource("view/EditCourse.fxml"));
 
+		AnchorPane modalPane = loader.load();
+		Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		Stage modal = initModal(modalPane, window);
 
-    /*
-     * initModal
-     *
-     * Initializes a new modal to appear over a darkened version of the current stage.
-     */
-    Stage initModal(Pane modalPane, Stage currentStage) {
-    	Scene modalScene = new Scene(modalPane);
-    	Stage modal = new Stage(StageStyle.TRANSPARENT);
-    	modal.initModality(Modality.WINDOW_MODAL);
-    	modal.initOwner(currentStage);
-    	modal.setScene(modalScene);
+		EditCourseController controller = loader.getController();
+		controller.passStages(modal, window);
 
-    	// darken current stage
-    	ColorAdjust darken = new ColorAdjust();
-    	darken.setBrightness(-0.5);
-        currentStage.getScene().getRoot().setEffect(darken);
+		// TODO: use EditCourse controller too pass in course info for edit
+		// ...
 
-        return modal;
-    }
+		modal.show();
+	}
 
+	/*
+	 * initModal
+	 *
+	 * Initializes a new modal to appear over a darkened version of the current
+	 * stage.
+	 */
+	Stage initModal(Pane modalPane, Stage currentStage) {
+		Scene modalScene = new Scene(modalPane);
+		Stage modal = new Stage(StageStyle.TRANSPARENT);
+		modal.initModality(Modality.WINDOW_MODAL);
+		modal.initOwner(currentStage);
+		modal.setScene(modalScene);
 
-  //-------------LIST VIEW FUNCTIONS----------------//
-    
-    
-    /*
-     * toggleTodo
-     * 
-     * Action when date toggle button is clicked.
-     * Calls loadTodo view to load the todo list.
-     */
-    @FXML
-    void toggleTodo(ActionEvent event) {
-    	loadTodoView();
-    }
-    
-    
-    /*
-     * toggleDate
-     * 
-     * Action when the date toggle button is clicked.
-     * Calls loadTodo view to load the date list.
-     */
-    @FXML
-    void toggleDate(ActionEvent event) {
-    	loadDateView();
-    }
-    
-    
-    /*
-     * loadTodoView
-     * 
-     * Load/display the list view(s) for the upcoming week of tasks
-     * and completed tasks.
-     */
-    void loadTodoView() {
-    	dateListView.setVisible(false);
+		// darken current stage
+		ColorAdjust darken = new ColorAdjust();
+		darken.setBrightness(-0.5);
+		currentStage.getScene().getRoot().setEffect(darken);
+
+		return modal;
+	}
+
+	// -------------LIST VIEW FUNCTIONS----------------//
+
+	/*
+	 * toggleTodo
+	 * 
+	 * Action when date toggle button is clicked. Calls loadTodo view to load the
+	 * todo list.
+	 */
+	@FXML
+	void toggleTodo(ActionEvent event) {
+		loadTodoView();
+	}
+
+	/*
+	 * toggleDate
+	 * 
+	 * Action when the date toggle button is clicked. Calls loadTodo view to load
+	 * the date list.
+	 */
+	@FXML
+	void toggleDate(ActionEvent event) {
+		loadDateView();
+	}
+
+	/*
+	 * loadTodoView
+	 * 
+	 * Load/display the list view(s) for the upcoming week of tasks and completed
+	 * tasks.
+	 */
+	void loadTodoView() {
+		dateListView.setVisible(false);
 		dateListView.setManaged(false);
-		
+
 		todoPane.setVisible(true);
 		todoPane.setManaged(true);
 		completedPane.setVisible(true);
 		completedPane.setManaged(true);
-    	
-    	todoList = FXCollections.observableArrayList();
-    	completedTasks = FXCollections.observableArrayList();
-    	
-    	// dummy data
-		todoList.addAll(
-    			new Task("task 1", "course 1", "12/12/12", "type 1"),
-    	        new Task("task 2", "course 2", "12/12/12", "type 2")
-    			);
-		
-		completedTasks.addAll(
-    			new Task("completed task 1", "course 1", "12/12/12", "type 1"),
-    	        new Task("completed task 2", "course 2", "12/12/12", "type 2")
-    			);
+
+		todoList = FXCollections.observableArrayList();
+		completedTasks = FXCollections.observableArrayList();
+
+		// dummy data
+		int size = MainModel.taskMap.size();
+		for (int i = 1; i < size; i++) {
+			todoList.add(MainModel.taskMap.get(i));
+		}
+
+		for(Task t : MainModel.completedQueue){
+			completedTasks.add(t);
+		}
 		
 		// add observable list to list view
 		todoListView.setItems(todoList);
 		completedListView.setItems(completedTasks);
-		
+
 		// customize the list view cells
-    	todoListView.setCellFactory(todoListView -> new TaskCell());
-    	completedListView.setCellFactory(completedListView -> new TaskCell());
-    	
-    }
-    
-    
-    /*
-     * loadDateView
-     * 
-     * Load/display the list view for tasks ordered by date
-     */
-    void loadDateView() {
-    	dateListView.setVisible(true);
+		todoListView.setCellFactory(todoListView -> new TaskCell());
+		completedListView.setCellFactory(completedListView -> new TaskCell());
+
+	}
+
+	/*
+	 * loadDateView
+	 * 
+	 * Load/display the list view for tasks ordered by date
+	 */
+	void loadDateView() {
+		dateListView.setVisible(true);
 		dateListView.setManaged(true);
-		
+
 		todoPane.setVisible(false);
 		todoPane.setManaged(false);
 		completedPane.setVisible(false);
 		completedPane.setManaged(false);
-    	
-    	listByDate = FXCollections.observableArrayList();
-    	
-    	// dummy data
-    	listByDate.addAll(
-    			new Task("task 1", "this is sorted by date", "12/12/12", "type 1"),
-    	        new Task("task 2", "course 2", "12/12/12", "type 2")
-    			);
-		
+
+		listByDate = FXCollections.observableArrayList();
+
+		// dummy data
+		listByDate.addAll(new Task("task 1", "this is sorted by date", "12/12/12", "type 1"),
+				new Task("task 2", "course 2", "12/12/12", "type 2"));
+
 		// add observable list to list view
 		dateListView.setItems(listByDate);
-		
+
 		// customize the list view cells
-    	dateListView.setCellFactory(dateListView -> new TaskCell());
-    }
-    
-    
-    /*
-     * loadEventsView
-     * 
-     * Load/display the list view for events
-     */
-    void loadEventsView() {
-    	events = FXCollections.observableArrayList();
-    	// dummy data
-		events.addAll(
-    			new Task("event 1", "course 1", "12/12/12", "type 1"),
-    	        new Task("event 2", "course 2", "12/12/12", "type 2")
-    			);
+		dateListView.setCellFactory(dateListView -> new TaskCell());
+	}
+
+	/*
+	 * loadEventsView
+	 * 
+	 * Load/display the list view for events
+	 */
+	void loadEventsView() {
+		events = FXCollections.observableArrayList();
+		// dummy data
+		events.addAll(new Task("event 1", "course 1", "12/12/12", "type 1"),
+				new Task("event 2", "course 2", "12/12/12", "type 2"));
 		// add observable list to list view
 		eventsListView.setItems(events);
-		
-    	// customize the list view cells
-    	eventsListView.setCellFactory(eventsListView -> new EventCell());
-    	
+
+		// customize the list view cells
+		eventsListView.setCellFactory(eventsListView -> new EventCell());
+
 		if (events.size() > 0) {
 			noEventsMsg.setVisible(false);
 			noEventsMsg.setManaged(false);
@@ -265,14 +255,16 @@ public class FrontPageController implements Initializable {
 			noEventsMsg.setVisible(true);
 			noEventsMsg.setManaged(true);
 		}
-    	
-    }
-    
-    
-  //-------------INITIALIZATION----------------//
-    
+
+	}
+
+	// -------------INITIALIZATION----------------//
+
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
+		TextModel.importFiles();
+		//MainModel.checkQueueDates();
+		
 		loadTodoView();
 		loadEventsView();
 	}
